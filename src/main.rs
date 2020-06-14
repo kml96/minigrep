8fb91bffd4b224d9f22c81ea9b8419a4188362cc
Kamal Chandra Upreti <kamalchettri1996@gmail.com>
2020-06-14 16:24:08 +0545

    Receive query text, filename and display them

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
+//use environment library
+use std::env;
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect(); // get arguments and store
+    let query = &args[1]; // query string to search
+    let filename = &args[2]; // filename to search
+
+    println!("query string is {}",query);
+    println!("filename to search is {}",filename);
 }
